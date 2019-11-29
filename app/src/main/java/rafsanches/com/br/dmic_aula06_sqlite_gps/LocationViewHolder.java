3f555ffd173a2343b8a391016c678c91c00cf407
Rafael <rafael.ssanches95@gmail.com>
2019-11-28 22:56:47 -0300

    Atividade aula 06 Sqlite

---
+++ app/src/main/java/rafsanches/com/br/dmic_aula06_sqlite_gps/LocationViewHolder.java
@@ -0,0 +1,12 @@
+package rafsanches.com.br.dmic_aula06_sqlite_gps;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LocationViewHolder {
+
+    ImageView localImageView;
+    TextView latitudeTextView;
+    TextView longitudeTextView;
+
+}
